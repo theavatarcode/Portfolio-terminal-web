2971fb61bfeed08a6c8d9ebd9fe0c5d16a757fd0
Tanakorn <oozoonteenoo1234@gmail.com>
2022-10-03 23:53:54 +0700

    firts commit

---
+++ commands.js
@@ -0,0 +1,86 @@
+help = [
+    'about         who am i ',
+    'skills        what can i do',
+    'projects      what did i do',
+    'social        where can u find me',
+    'github        visit my github',
+    'secret        find my password',
+    'history       View commands history',
+    'banner        Show my banner',
+    'email         contact me',
+    'clear         clear terminal'
+
+]
+
+banner = [
+    '<span class="index">Portfolio about Tanakorn Sornubon</span>',
+    '                                                                                                                                       ',
+   "██████╗  ██████╗ ██████╗ ████████╗███████╗ ██████╗ ██╗     ██╗ ██████╗                        MMM8&&&.                                  ",
+   "██╔══██╗██╔═══██╗██╔══██╗╚══██╔══╝██╔════╝██╔═══██╗██║     ██║██╔═══██╗                 _...MMMMM88&&&&..._                                  ",
+   "██████╔╝██║   ██║██████╔╝   ██║   █████╗  ██║   ██║██║     ██║██║   ██║              .::'''MMMMM88&&&&&&'''::.                                  ",
+   "██╔═══╝ ██║   ██║██╔══██╗   ██║   ██╔══╝  ██║   ██║██║     ██║██║   ██║             ::     MMMMM88&&&&&&     ::                                 ",
+   "██║     ╚██████╔╝██║  ██║   ██║   ██║     ╚██████╔╝███████╗██║╚██████╔╝             '::....MMMMM88&&&&&&....::'                                 ",
+   "╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝      ╚═════╝ ╚══════╝╚═╝ ╚═════╝                 `''''MMMMM88&&&&''''`                                 ",
+   "                                                                                             'MMM8&&&'                                               ",
+   '<span class="color2">Welcome to my portfolio web terminal.</span>',
+   '<span class="color2">For a list of available commands, type</span><span class="command"> "help"</span><br>'
+   
+]
+
+about = [
+    '<br>',
+    '|_____________________|                      ',
+    '|  _________________  |                      ',
+    '| |   > Aboumt Me   | |            <i class="fa-solid fa-code"></i>          ',
+    '| |_________________| |            --------------------------------------------------       ',
+    '|  __ __ __ __ __ __  |            <i class="fa-solid fa-address-card"></i>  PROFILE     ',
+    '| |__|__|__|__|__|__| |            <i class="fa-solid fa-user"></i> NAME : Tanakorn Sornubon                                                    ',
+    '| |__|__|__|__|__|__| |            <i class="fa-solid fa-user"></i> CALLME : NEWTON                                                      ',
+    '| |__|__|__|__|__|__| |            <i class="fa-solid fa-user"></i> AGE : 18                                                         ',
+    '| |__|__|__|__|__|__| |            <i class="fa-solid fa-location-dot"></i> FROM : MVK Prachinburi                                                          ',
+    '| |__|__|__|__|__|__| |            --------------------------------------------------                                                             ',
+    '| |__|__|__|__|__|__| |            <i class="fa-solid fa-address-book"></i>  CONTACT                                                            ',
+    '|  ___ ___ ___   ___  |            <i class="fa-brands fa-facebook"></i> <a href="https://www.facebook.com/profile.php?id=100017824475556" target="_blank">Tanakorn Sornubon</a>                                                          ',
+    '| | 7 | 8 | 9 | | + | |            <i class="fa-brands fa-instagram"></i> <a href="https://www.instagram.com/newt_nn/" target="_blank">newt_nn</a>                                                           ',
+    '| |___|___|___| |___| |            <i class="fa-brands fa-github"></i> <a href="https://github.com/theavatarcode" target="_blank">theavatarcode</a>                                                            ',
+    '| | 4 | 5 | 6 | | - | |            <i class="fa-solid fa-phone"></i> <a href="#" target="_blank">081-2319434</a>                                                              ',
+    '| |___|___|___| |___| |            --------------------------------------------------                                                             ',
+    '| | 1 | 2 | 3 | | x | |            <i class="fa-solid fa-globe"></i>  My Website                                                             ',
+    '| |___|___|___| |___| |            <i class="fa-solid fa-book"></i> <a href="https://realtimechat-application-nt.herokuapp.com/" target="_blank">Chat Application</a>                                                                 ',
+    '| | . | 0 | = | | / | |            <i class="fa-solid fa-book"></i> <a href="https://memory-game-nt.netlify.app/" target="_blank">Memory Game</a>                                                                 ',
+    '| |___|___|___| |___| |            --------------------------------------------------                                                                 ',
+    '|_____________________|            <i class="fa-solid fa-code"></i>                                                                ',
+    '<br>'
+]
+
+
+secret = [
+    'Permission confirm     ',
+    '------------------     ',
+    '       sudo            '
+]
+
+
+skills = [
+    '                                                          ',
+    '                                                          ',
+    '             <i class="fa-solid fa-code"></i>                                 ',
+    '             ------------------------------                                  ',
+    '             <i class="fa-brands fa-python"></i> Python                                   ',
+    '             <i class="fa-brands fa-square-js"></i> Javascript                                   ',
+    '             <i class="fa-brands fa-css3-alt"></i> CSS                                   ',
+    '             <i class="fa-brands fa-html5"></i> HTML                                   ',
+    '             <i class="fa-brands fa-php"></i> PHP                                   ',
+    '             <i class="fa-solid fa-database"></i> MYSQL                                   ',
+    '             <i class="fa-brands fa-node-js"></i> NodeJS                                   ',
+    '             <i class="fa-solid fa-microchip"></i> Machine Learning                                   ',
+    '             -------------------------------                                   ',
+    '             <i class="fa-solid fa-code"></i>                                   ',
+    '                                                          ',
+    '                                                          ',
+
+]
+
+
+                                                                   
+
